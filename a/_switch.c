181d69613de3ab9a86978e1194c334b5a6219827
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-12 18:16:45 +0000

    Tabination fix

--- a/_switch.c
+++ a/_switch.c
@@ -15,29 +15,29 @@ int _switch(const char *format, va_list pa1, int index, int *counter)
 		index = space(format, index + 1);
 	switch (format[index + 1])
 	{
-		case 'c':
-			write_char(va_arg(pa1, int));
-			(*counter)++;
-			break;
-		case 's':
-			string = va_arg(pa1, char*);
-			write_string(string);
-			(*counter) += _strlen(string);
-			break;
-		case 'i':
-			(*counter) += write_number(va_arg(pa1, int));
-			break;
-		case 'd':
-			(*counter) += write_number(va_arg(pa1, int));
-			break;
-		case '%':
-			write_char('%');
-			(*counter)++;
-			break;
-		default:
-			write_char('%');
-			index--;
-			break;
+	case 'c':
+		write_char(va_arg(pa1, int));
+		(*counter)++;
+		break;
+	case 's':
+		string = va_arg(pa1, char *);
+		write_string(string);
+		(*counter) += _strlen(string);
+		break;
+	case 'i':
+		(*counter) += write_number(va_arg(pa1, int));
+		break;
+	case 'd':
+		(*counter) += write_number(va_arg(pa1, int));
+		break;
+	case '%':
+		write_char('%');
+		(*counter)++;
+		break;
+	default:
+		write_char('%');
+		index--;
+		break;
 	}
 	return (index);
 }
